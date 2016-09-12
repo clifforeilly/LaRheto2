88c3a3ebd81f78aea7d96455338b159e501f2949
cliffo <clifforeilly@gmail.com>
2016-09-12 21:15:59 +0100

    Initial commit

---
+++ src/control.java
@@ -0,0 +1,20 @@
+/**
+ * Created by cliff on 12/09/2016.
+ */
+public class control {
+
+
+    public void main (String[] args){
+
+        System.out.println("starting ...");
+
+
+
+
+
+
+        System.out.println("ending ...");
+
+    }
+
+}
